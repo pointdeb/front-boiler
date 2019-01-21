f9dbe7e00d3a1dac95d56b34e2a38e86c69e1454
Stephano Romain <stephano.romain@gmail.com>
2019-01-21 12:10:57 +0000

    bug fix

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ tabs = [...tabs, 'world']
 
 console.log(tabs)
 
+// document.getElementsByClassName('lead')[0].innerHTML = tabs.join(' ')
+
 document.querySelector('.lead').innerHTML = tabs.join(' ')
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,25 @@
 const path = require('path')
 
 const dev = process.env.NODE_ENV !== 'production'
-const assetPath = './dist';
+const pathsToClean = [ 'dist', 'build' ]
+const cleanOptions = {
+  exclude: ['.htaccess'],
+  verbose: dev,
+  dry: !dev,
+}
+// const assetPath = './dist'
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const config = {
-  entry: ['./src/index.html', './src/style.scss', './src/index.js'],
+  entry: {
+    vendor: ['@babel/polyfill'],
+    main: ['./src/style.scss', './src/index.js']
+  },
   output: {
     path: path.resolve('dist'),
-    filename: '[name].js',
+    filename: '[name].js'
     // publicPath: dev ? `http://localhost:8080` : '/'
   },
   mode: dev ? 'development' : 'production',
@@ -21,17 +30,16 @@ const config = {
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization'
     },
     overlay: true,
     clientLogLevel: 'warning'
     // host: 0.0.0.0
   },
-  resolve:{
+  resolve: {
     extensions: ['.js', '.jsx', '.css', '.scss', '.json', '.xml', '.svg', '.html'],
-    alias: {
-      '~': path.resolve('node_modules') + '/'
-    }
+    alias: { '~': path.resolve('node_modules') + '/' }
   },
   module: {
     rules: [
@@ -52,7 +60,7 @@ const config = {
         test: /\.html/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'html-loader', 
+          loader: 'html-loader',
           options: {
             minimize: true
           }
@@ -68,7 +76,7 @@ const config = {
               options: {
                 minimize: !dev,
                 sourceMap: dev
-              },
+              }
             },
             {
               loader: 'postcss-loader',
@@ -89,20 +97,21 @@ const config = {
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
-        },
-      },
+          limit: 10000
+        }
+      }
     ]
   },
   plugins: [
     new ExtractTextPlugin({
-      filename: 'style.css',
+      filename: '[name].css'
     }),
     new HtmlWebpackPlugin({
       template: path.resolve('./src/index.html'),
       filename: 'index.html',
-      children: false, 
-    })
+      children: false
+    }),
+    new CleanWebpackPlugin(pathsToClean, cleanOptions)
   ]
 }
 
